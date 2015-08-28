70c306e16413e03ee5704d0ad0ddd546abe6eafa
Vipul Swarup <vipul.swarup@eisenvault.com>
2015-08-28 16:05:24 +0530

    Enabled parallel uploads

--- upload-google-storage.sh
+++ upload-google-storage.sh
@@ -3,4 +3,4 @@ value=$(</opt/backup/bakfile.txt)
 echo "Bak file name is $value \n"
 echo "Uploading $value"
 #Copy backup to google cloud bucket
-gsutil cp $value gs://ev-live-backup/
+gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp $value gs://ev-live-backup/
